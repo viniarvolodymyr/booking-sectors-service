d6ec52ef32834d106ba4b511007d8ca85bc1e2e8
SviatoslavSkobalski <54236099+SviatoslavSkobalski@users.noreply.github.com>
2019-12-06 18:05:26 +0200

    Update IUnitOfWork.cs

--- SoftServe.BookingSectors.WebAPI/DAL/UnitOfWork/IUnitOfWork.cs
+++ SoftServe.BookingSectors.WebAPI/DAL/UnitOfWork/IUnitOfWork.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using SoftServe.BookingSectors.WebAPI.DAL.Models;
 using SoftServe.BookingSectors.WebAPI.DAL.Repositories;
+using SoftServe.BookingSectors.WebAPI.DAL.Repositories.Interfaces;
 
 namespace SoftServe.BookingSectors.WebAPI.DAL.UnitOfWork
 {
     public interface IUnitOfWork : IDisposable
     {
         IBaseRepository<Sector> Sectors { get; }
+        ISettingsRepository settings { get;  }
         void Save();
     }
 }
