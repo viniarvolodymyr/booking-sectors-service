c976247ce716fd21894c6f35adce0f5270d0440a
Volodymyr Viniar <viniar.volodymyr@gmail.com>
2019-12-06 18:28:13 +0200

    Add BookingSectorDTO class

---
+++ SoftServe.BookingSectors.WebAPI/BLL/DTO/BookingSectorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftServe.BookingSectors.WebAPI.BLL.DTO
+{
+    public class BookingSectorDTO
+    {
+        public DateTime BookingStart { get; set; }
+        public DateTime BookingEnd { get; set; }
+        public bool? IsApproved { get; set; }
+        public int CreateUserId { get; set; }
+    }
+}
