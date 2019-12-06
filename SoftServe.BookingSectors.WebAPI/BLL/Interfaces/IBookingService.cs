ea8af75695ccb955cdea7faf26fb0215a5a0c294
Volodymyr Viniar <viniar.volodymyr@gmail.com>
2019-12-06 13:21:42 +0200

    Add IBookingService interface

---
+++ SoftServe.BookingSectors.WebAPI/BLL/Interfaces/IBookingService.cs
@@ -0,0 +1,18 @@
+﻿using SoftServe.BookingSectors.WebAPI.BLL.DTO;
+using SoftServe.BookingSectors.WebAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SoftServe.BookingSectors.WebAPI.BLL.Interfaces
+{
+    public interface IBookingService
+    {
+        Task<IEnumerable<BookingSector>> GetBookingSectorsAsync();
+        Task<BookingSector> GetBookingByIdAsync();
+        Task<IEnumerable<SectorDTO>> GetFreeSectorsAsync(DateTime fromDate, DateTime toDate);
+        void UpdateBookingApproved(int id, bool isApproved);
+        void DeleteBookingById(int id);
+        void Dispose();
+    }
+}
