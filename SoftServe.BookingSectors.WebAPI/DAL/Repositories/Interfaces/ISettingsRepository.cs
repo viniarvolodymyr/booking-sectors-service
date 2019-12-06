06afa0d36a087d98af7ee6e8854dd36a3e197f48
SviatoslavSkobalski <54236099+SviatoslavSkobalski@users.noreply.github.com>
2019-12-06 18:03:06 +0200

    Create ISettingsRepository.cs

---
+++ SoftServe.BookingSectors.WebAPI/DAL/Repositories/Interfaces/ISettingsRepository.cs
@@ -0,0 +1,14 @@
+﻿using SoftServe.BookingSectors.WebAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftServe.BookingSectors.WebAPI.DAL.Repositories.Interfaces
+{
+    public interface ISettingsRepository
+    {
+            public Setting GetSetting(string name);
+            public void UpdateSettings(string name1, string name2, int value1, int value2);
+        }
+}
