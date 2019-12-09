96e463fab6cdb450d2d941f64d2bc4f7aa7049cc
SviatoslavSkobalski <54236099+SviatoslavSkobalski@users.noreply.github.com>
2019-12-09 14:05:37 +0200

    Update SettingsController.cs

--- SoftServe.BookingSectors.WebAPI/Controllers/SettingsController.cs
+++ SoftServe.BookingSectors.WebAPI/Controllers/SettingsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SoftServe.BookingSectors.WebAPI.BLL.DTO;
 using SoftServe.BookingSectors.WebAPI.DAL.Models;
 using SoftServe.BookingSectors.WebAPI.DAL.Repositories.Interfaces;
 
@@ -13,15 +14,15 @@ namespace SoftServe.BookingSectors.WebAPI.Controllers
     [ApiController]
     public class SettingsController : ControllerBase
     {
-        private readonly ISettingsRepository _settings;
-        public SettingsController(ISettingsRepository settings)
+        private readonly ISettingsService _settings;
+        public SettingsController(ISettingsService settings)
         {
             _settings = settings;
         }
         [HttpGet("{name}", Name = "GetSetting")]
-        public Setting Get(string name)
+        public Task<SettingsDTO> Get(string name)
         {
-            return _settings.GetSetting(name);
+            return _settings.GetSettingByIdAsync(name);
         }
         [HttpPut("{name1}, {name2}, {value1}, {value2}", Name = "Update setting")]
         public void Update(string name1, string name2, int value1, int value2)
