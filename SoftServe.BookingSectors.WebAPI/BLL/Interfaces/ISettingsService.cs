242d8a437ea27c14e77e95d36000b77ede678775
SviatoslavSkobalski <54236099+SviatoslavSkobalski@users.noreply.github.com>
2019-12-09 14:05:22 +0200

    Create ISettingsService.cs

---
+++ SoftServe.BookingSectors.WebAPI/BLL/Interfaces/ISettingsService.cs
@@ -0,0 +1,16 @@
+﻿using SoftServe.BookingSectors.WebAPI.BLL.DTO;
+using SoftServe.BookingSectors.WebAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftServe.BookingSectors.WebAPI.DAL.Repositories.Interfaces
+{
+    public interface ISettingsService
+    {
+        Task<SettingsDTO> GetSettingByIdAsync(string name);
+        public void UpdateSettings(string name1, string name2, int value1, int value2);
+        void Dispose();
+    }
+}
