d064b403cd08788068e56e086c3dfcdcfc9a8289
SviatoslavSkobalski <54236099+SviatoslavSkobalski@users.noreply.github.com>
2019-12-09 14:05:18 +0200

    Create SettingsDTO.cs

---
+++ SoftServe.BookingSectors.WebAPI/BLL/DTO/SettingsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftServe.BookingSectors.WebAPI.BLL.DTO
+{
+    public class SettingsDTO
+    {
+        public string name { get; set; }
+        public int value { get; set; }
+    }
+}
