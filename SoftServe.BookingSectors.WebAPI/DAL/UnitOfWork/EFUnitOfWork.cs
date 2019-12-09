e34bf5a0f68218679580ce63a89b9737465eb489
SviatoslavSkobalski <54236099+SviatoslavSkobalski@users.noreply.github.com>
2019-12-09 17:25:40 +0200

    Update EFUnitOfWork.cs

--- SoftServe.BookingSectors.WebAPI/DAL/UnitOfWork/EFUnitOfWork.cs
+++ SoftServe.BookingSectors.WebAPI/DAL/UnitOfWork/EFUnitOfWork.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SoftServe.BookingSectors.WebAPI.DAL.EF;
 using SoftServe.BookingSectors.WebAPI.DAL.Models;
 using SoftServe.BookingSectors.WebAPI.DAL.Repositories;
