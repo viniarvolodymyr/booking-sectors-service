5e9cf08394e37fcd1b040d7da02720b41ae89cc5
Yura <41750763+zapletnyuk@users.noreply.github.com>
2019-12-19 17:54:36 +0200

    Added foreign key Tournament_ID to booking_sector table and recreated new models

--- SoftServe.BookingSectors.WebAPI/DAL/Models/Tournament.cs
+++ SoftServe.BookingSectors.WebAPI/DAL/Models/Tournament.cs
@@ -5,6 +5,12 @@ namespace SoftServe.BookingSectors.WebAPI.DAL.Models
 {
     public partial class Tournament
     {
+        public Tournament()
+        {
+            BookingSector = new HashSet<BookingSector>();
+            TournamentSector = new HashSet<TournamentSector>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime DateStart { get; set; }
@@ -15,6 +21,7 @@ namespace SoftServe.BookingSectors.WebAPI.DAL.Models
         public DateTime ModDate { get; set; }
         public int? ModUserId { get; set; }
 
-        public virtual ICollection<TournamentSector> TournamentSector { get; set; } = new HashSet<TournamentSector>();
+        public virtual ICollection<BookingSector> BookingSector { get; set; }
+        public virtual ICollection<TournamentSector> TournamentSector { get; set; }
     }
 }
--- SoftServe.BookingSectors.WebAPI/DAL/Models/UserRole.cs
+++ SoftServe.BookingSectors.WebAPI/DAL/Models/UserRole.cs
@@ -5,6 +5,11 @@ namespace SoftServe.BookingSectors.WebAPI.DAL.Models
 {
     public partial class UserRole
     {
+        public UserRole()
+        {
+            User = new HashSet<User>();
+        }
+
         public int Id { get; set; }
         public string Role { get; set; }
         public DateTime CreateDate { get; set; }
@@ -12,6 +17,6 @@ namespace SoftServe.BookingSectors.WebAPI.DAL.Models
         public DateTime ModDate { get; set; }
         public int? ModUserId { get; set; }
 
-        public virtual ICollection<User> User { get; set; } = new HashSet<User>();
+        public virtual ICollection<User> User { get; set; }
     }
 }
