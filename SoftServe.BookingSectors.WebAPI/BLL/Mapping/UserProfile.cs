74c865b4b603eafee729773e3451e62401eb78e1
Andrew Voisovych <andr.vois@gmail.com>
2019-12-27 09:03:56 +0200

    add Email send

--- SoftServe.BookingSectors.WebAPI/BLL/Mapping/UserProfile.cs
+++ SoftServe.BookingSectors.WebAPI/BLL/Mapping/UserProfile.cs
@@ -10,13 +10,23 @@ namespace SoftServe.BookingSectors.WebAPI.BLL.Mapping
         {
             CreateMap<User, UserDTO>()
                 .ForMember(m => m.RoleName, x => x.MapFrom(src => src.Role.Role));
+                
             CreateMap<UserDTO, User>()
                 .ForMember(m => m.Id, opt => opt.Ignore())
                 .ForMember(m => m.Role, opt => opt.Ignore())
                 .ForMember(m => m.Password, opt => opt.Ignore())
+                .ForMember(m => m.Photo, opt => opt.Ignore());  
+
+
+            CreateMap<User, RegistrationDTO>()
+                .ForMember(m => m.RoleName, x => x.MapFrom(src => src.Role.Role))
+                .ForMember(m => m.Email, opt => opt.Ignore());
+                
+            CreateMap<RegistrationDTO, User>()
+                .ForMember(m => m.Id, opt => opt.Ignore())
+                .ForMember(m => m.Role, opt => opt.Ignore())
+                .ForMember(m => m.Password, opt => opt.Ignore())
                 .ForMember(m => m.Photo, opt => opt.Ignore());
-
-
         }
     }
 }
--- SoftServe.BookingSectors.WebAPI/DAL/EF/BookingSectorContext.cs
+++ SoftServe.BookingSectors.WebAPI/DAL/EF/BookingSectorContext.cs
@@ -15,6 +15,7 @@ namespace SoftServe.BookingSectors.WebAPI.DAL.EF
         }
 
         public virtual DbSet<BookingSector> BookingSector { get; set; }
+        public virtual DbSet<Email> Email { get; set; }
         public virtual DbSet<Language> Language { get; set; }
         public virtual DbSet<Sector> Sector { get; set; }
         public virtual DbSet<Setting> Setting { get; set; }
@@ -79,6 +80,25 @@ namespace SoftServe.BookingSectors.WebAPI.DAL.EF
                     .HasConstraintName("FK_USER_ID");
             });
 
+            modelBuilder.Entity<Email>(entity =>
+            {
+                entity.ToTable("EMAIL");
+
+                entity.HasIndex(e => e.UserId)
+                    .HasName("UQ__EMAIL__F3BEEBFE9FCE3036")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Email1)
+                    .IsRequired()
+                    .HasColumnName("EMAIL")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserId).HasColumnName("USER_ID");
+            });
+
             modelBuilder.Entity<Language>(entity =>
             {
                 entity.ToTable("LANGUAGE");
@@ -108,39 +128,6 @@ namespace SoftServe.BookingSectors.WebAPI.DAL.EF
                     .HasColumnName("NAME")
                     .HasMaxLength(25)
                     .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<Token>(entity =>
-            {
-                entity.ToTable("TOKEN");
-
-                entity.Property(e => e.Id).HasColumnName("ID");
-
-                entity.Property(e => e.CreateDate)
-                    .HasColumnName("CREATE_DATE")
-                    .HasColumnType("datetime")
-                    .HasDefaultValueSql("(getdate())");
-
-                entity.Property(e => e.CreateId).HasColumnName("CREATE_ID");
-
-                entity.Property(e => e.ModDate)
-                    .HasColumnName("MOD_DATE")
-                    .HasColumnType("datetime")
-                    .HasDefaultValueSql("(getdate())");
-
-                entity.Property(e => e.ModId).HasColumnName("MOD_ID");
-
-                entity.Property(e => e.RefreshToken).HasColumnName("REFRESH_TOKEN");
-
-                entity.HasOne(d => d.Create)
-                    .WithMany(p => p.TokenCreate)
-                    .HasForeignKey(d => d.CreateId)
-                    .HasConstraintName("FK_CREATE_TOKEN_USER");
-
-                entity.HasOne(d => d.Mod)
-                    .WithMany(p => p.TokenMod)
-                    .HasForeignKey(d => d.ModId)
-                    .HasConstraintName("FK_MOD_TOKEN_USER");
             });
 
             modelBuilder.Entity<Sector>(entity =>
@@ -216,6 +203,39 @@ namespace SoftServe.BookingSectors.WebAPI.DAL.EF
                     .IsUnicode(false);
 
                 entity.Property(e => e.Value).HasColumnName("VALUE");
+            });
+
+            modelBuilder.Entity<Token>(entity =>
+            {
+                entity.ToTable("TOKEN");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreateDate)
+                    .HasColumnName("CREATE_DATE")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.CreateId).HasColumnName("CREATE_ID");
+
+                entity.Property(e => e.ModDate)
+                    .HasColumnName("MOD_DATE")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ModId).HasColumnName("MOD_ID");
+
+                entity.Property(e => e.RefreshToken).HasColumnName("REFRESH_TOKEN");
+
+                entity.HasOne(d => d.Create)
+                    .WithMany(p => p.TokenCreate)
+                    .HasForeignKey(d => d.CreateId)
+                    .HasConstraintName("FK_CREATE_TOKEN_USER");
+
+                entity.HasOne(d => d.Mod)
+                    .WithMany(p => p.TokenMod)
+                    .HasForeignKey(d => d.ModId)
+                    .HasConstraintName("FK_MOD_TOKEN_USER");
             });
 
             modelBuilder.Entity<Tournament>(entity =>
