af6a5251b8e773a18902eb44b9fd459d2fd718d3
Liliia Bekirova <lilybekirova@gmail.com>
2020-01-15 07:45:55 +0200

    changed mapping for bookingSector

--- SoftServe.BookingSectors.WebAPI/BLL/Mapping/BookingSectorProfile.cs
+++ SoftServe.BookingSectors.WebAPI/BLL/Mapping/BookingSectorProfile.cs
@@ -10,7 +10,6 @@ namespace SoftServe.BookingSectors.WebAPI.BLL.Mapping
         {
             CreateMap<BookingSector, BookingSectorDTO>();
             CreateMap<BookingSectorDTO, BookingSector>()
-                .ForMember(m => m.IsApproved, opt => opt.Ignore())
                 .ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
